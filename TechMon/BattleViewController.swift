7866bc4636528f6435abdf8f2f6b50def0da075d
daisukesone <sonedai0213@gmail.com>
2018-02-13 12:30:21 +0900

    step2まで

--- TechMon/BattleViewController.swift
+++ TechMon/BattleViewController.swift
@@ -9,11 +9,151 @@
 import UIKit
 
 class BattleViewController: UIViewController {
+    
+    @IBOutlet var playerNameLabel: UILabel!
+    @IBOutlet var playerImageView: UIImageView!
+    @IBOutlet var playerHPLabel: UILabel!
+    @IBOutlet var playerMPLabel: UILabel!
+    
+    @IBOutlet var enemyNameLabel: UILabel!
+    @IBOutlet var enemyImageView: UIImageView!
+    @IBOutlet var enemyHPLabel: UILabel!
+    @IBOutlet var enemyMPLabel: UILabel!
+    
+    let techMonManager = TechMonManager.shared
+    
+    var playerHP = 100
+    var playerMP = 0
+    var enemyHP = 100
+    var enemyMP = 0
+    
+    var gameTimer: Timer!
+    var isPlayerAttackAvalable: Bool = true
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //プレイヤーのステータスを反映
+        playerNameLabel.text  = "勇者"
+        playerImageView.image = UIImage(named: "yusha.png")
+        playerHPLabel.text = "\(playerHP)/ 100"
+        playerMPLabel.text = "\(playerMP)/ 20"
+        
+        //敵のステータスを反映
+        enemyNameLabel.text  = "勇者"
+        enemyImageView.image = UIImage(named: "monster.png")
+        enemyHPLabel.text = "\(enemyHP)/ 200"
+        enemyMPLabel.text = "\(enemyMP)/ 35"
+        
+        //ゲームスタート
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateGame), userInfo: nil, repeats: true)
+        
+        gameTimer.fire()
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        techMonManager.playBGM(fileName: "BGM_battle001")
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        TechMonManager.stopBGM()
+    }
+    
+    //0.1秒ごとにゲームの状態を更新する
+    func updateaGame(){
+        //プレイヤーのステータスを更新
+        playerMP += 1
+        if playerMP >= 20 {
+            
+            isPlayerAttackAvalable = true
+            playerMP = 20
+        } else {
+            
+            isPlayerAttackAvalable = false
+        }
+        
+        //敵のステータスを更新
+        enemyMP += 1
+        if enemyMP >= 35 {
+            
+            enemyAttack()
+            enemyMP = 0
+        }
+        
+        playerMPLabel.text = "\(playerMP)/ 20"
+        enemyMPLabel.text = "\(enemyMP)/ 35"
+    }
+    
+    //敵の攻撃
+    func enemyAttack(){
+        
+        techMonManager.damageAnimation(imageView: playerImageView)
+        techMonManager.playSE(fileName: "SE_attack")
+        
+        playerHP -= 20
+        playerHPLabel.text = "\(playerHP)/100"
+        if playerHP <= 0 {
+            finishBattle(vanishImageView: playerMPLabel, isPlayerwin: false)
+            
+        }
+    }
+    
+    //勝敗が決定した時の処理
+    func finishBattle(vanishImageView: UIImageView, isPlayerwin: Bool){
+        
+        techMonManager.vanishAnimation(imageView: vanishImageView)
+        techMonManager.stopBGM()
+        gameTimer.invalidate()
+        isPlayerAttackAvalable = false
+        
+        var finishMessage: String = ""
+        if  isPlayerWin {
+            
+            techMonManager.playSE(fileName: "SE_fanfare")
+            finishMessage = "勇者の勝利！！"
+        } else {
+            
+            techMonManager.playSE(fileName: "SE_gameover")
+            finishMessage = "勇者の敗北..."
+            
+        }
+    
+    
+       let alert = UIAlertController(title:"バトル終了", message: finishMessage, prefferedstyle: .alert)
+       alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+    
+          self.dismiss(animated: true, completion: nil
+        })))
+       present(alert, animated: true, completion: nil)
+   
+    }
+    
+    
+    @IBAction func atttackAction(){
+        if isPlayerAttackAvalable {
+            
+            techMonManager.damageAnimation(imageView: enemyImageView)
+            techMonManager.playerSE(fileName: "SE_attack")
+            
+            enemyHP -= 30
+            playerMP = 0
+            
+            enemyHPLabel.text = "\(enemyHP)/ 200"
+            playerMPLabel.text = "\(playerMP)/20"
+            
+            if enemyHP <= 0 {
+                finishBattle(vanishImageView: enemyImageView, isPlayerWin: true)
+            }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +172,5 @@ class BattleViewController: UIViewController {
     }
     */
 
+}
 }
--- TechMon/LobbyViewController.swift
+++ TechMon/LobbyViewController.swift
@@ -15,13 +15,13 @@ class LobbyViewController: UIViewController {
     
     let techMonManager = TechMonManager.shared
     
+    
     var stamina: Int = 100
-    @objc var staminaTimer: Timer!
+    var staminaTimer: Timer!
     
-    
-    
-
+    //アプリが起動したとき一度だけ呼ばれる
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         //UIの設定
@@ -29,7 +29,11 @@ class LobbyViewController: UIViewController {
         staminaLabel.text = "\(stamina)/100"
         
         //タイマーの設定
-        staminaTimer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(self.staminaTimer), userInfo: nil, repeats: true)
+        staminaTimer = Timer.scheduledTimer(timeInterval: 3,
+                                            target: self,
+                                            selector: #selector(updateStaminaValue),
+                                            userInfo: nil,
+                                            repeats: true)
         
         staminaTimer.fire()
     }
@@ -37,7 +41,44 @@ class LobbyViewController: UIViewController {
     //ロビー画面が見えるようになるときに呼ばれる
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        techMonManager.playBGM(fileName: "lobby")
+    }
+    
+    //ロビー画面が見えなくなったときに呼ばれる
+    override func viewWillDisappear(_ animated: Bool){
+        super.viewWillDisappear(animated)
         techMonManager.stopBGM()
+        
+    }
+    
+    @IBAction func toBattle() {
+        //スタミナが50以上あればスタミナ50を消費して戦闘画面へ
+        if stamina >= 50 {
+            
+            stamina -= 50
+            staminaLabel.text  = "\(stamina)/100"
+            performSegue(withIdentifier: "toBattle", sender: nil)
+        } else {
+            
+            let alert = UIAlertController(
+                title: "バトルに行けません",
+                message: "スタミナをためてください",
+                preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK.", style: .default, handler: nil))
+            present(alert,animated: true, completion: nil)
+            
+            
+        }
+    }
+    
+    //スタミナの回復
+    @objc func updateStaminaValue() {
+        if stamina < 100{
+            
+            stamina += 1
+            staminaLabel.text = "\(stamina)/100"
+            
+        }
     }
     
 
